53a0be952070c57db85694cfd0ac0374f058f97b
Mohamed Reda <alprofmohamed123@gmail.com>
2022-01-04 22:17:50 +0200

    All my repos of github on this site you can change my link with your and use it without any credits.

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
 // we will make small project with js only without anything in html or css all will be here in js file
 // first to make the header
+const getFirstRepo = (apiURL) => {
+    return new Promise(((resolve, reject) => {
+        let myRequest = new XMLHttpRequest();
+        myRequest.onload = function () {
+            if (this.readyState === 4 && this.status === 200){
+                resolve(JSON.parse(this.responseText))
+            }else {
+                reject(Error(this.statusText))
+            }
+        }
+        myRequest.open("GET", apiURL, true);
+        myRequest.send();
+    }))
+}
+getFirstRepo("https://api.github.com/users/MohamedReda98/repos")
+    .then((result) =>{
+        append(result);
+    })
+    .catch((error)=>{
+        console.log(error);
+    });
 let header = document.createElement('header');
 let content = document.createElement('section');
 let footer = document.createElement('footer');
@@ -30,47 +51,59 @@ header.appendChild(menu);
 header.style.cssText='display:flex;padding:5px;justify-content:space-between;'
 document.body.appendChild(header);
 // Adding the content to the shape
-for (let i = 1; i<=15 ;i++){
-// make the main elements
-    let myMainDiv =document.createElement("div");
-    let mySpan =document.createElement("span");
-// make the main text nodes
-    let textDiv = document.createTextNode(`Product`);
-    let textSpan = document.createTextNode(`${i}`);
-    // set the class attribute and assign its value
-    myMainDiv.setAttribute("class","product");
-    myMainDiv.style.cssText="padding: 20px;background-color:#eee; color:gray;\n" +
-        "\twidth: 150px;\n" +
-        "\tdisplay: flex;\n" +
-        "\tflex-direction: column;\n" +
-        "\talign-items: center;\n" +
-        "\ttext-align: center;\n" +
-        "\tborder-radius: .5rem;\n" +
-        "\tborder-bottom: 2px solid blue;\n" +
-        "\tbox-shadow: 0 10px 40px -14px rgba(0,0,0,0.25);";
-    mySpan.style.cssText="font-size: 1rem;\n" +
-        "\tfont-weight: 600;\n" +
-        "\tmargin: 0.7rem 0 0.4rem;color:black";
-// adding them to the view menu
-    mySpan.appendChild(textSpan);
-// adding them to the main div
-    myMainDiv.appendChild(mySpan);
-    myMainDiv.appendChild(textDiv);
-    content.appendChild(myMainDiv);
-    // document.body.appendChild(breaking);
-    content.style.cssText="display: grid;\n" +
-        "\tgrid-template-columns: repeat(auto-fit,minmax(200px, auto));\n" +
-        "\tjustify-content: center;\n" +
-        "\tgap: 2rem;\n" +
-        "\tmargin: 2rem;";
-}
-document.body.appendChild(content);
+function append(arr) {
+    setTimeout(()=>{
+        arr.forEach((value) => {
+            // make the main elements
+            let myMainDiv =document.createElement("div");
+            let mySpan =document.createElement("span");
+            let myLink =document.createElement("a");
+            // make the main text nodes
+            let textDiv = document.createTextNode(`Language: ${value.language}`);
+            let textSpan = document.createTextNode(`${value.name}`);
+            let linkSpan = document.createTextNode(`link`);
+            // set the class attribute and assign its value
+            myMainDiv.setAttribute("class","product");
+            myLink.setAttribute("href",`${value['html_url']}`);
+            myLink.setAttribute("target","_blank");
+            myMainDiv.style.cssText="padding: 20px;background-color:#eee; color:gray;\n" +
+                "\twidth: 150px;\n" +
+                "\tdisplay: flex;\n" +
+                "\tflex-direction: column;\n" +
+                "\talign-items: center;\n" +
+                "\ttext-align: center;\n" +
+                "\tborder-radius: .5rem;\n" +
+                "\tborder-bottom: 2px solid blue;\n" +
+                "\tbox-shadow: 0 10px 40px -14px rgba(0,0,0,0.25);";
+            mySpan.style.cssText="font-size: 1rem;\n" +
+                "\tfont-weight: 600;\n" +
+                "\tmargin: 0.7rem 0 0.4rem;color:black";
+            // adding them to the view menu
+            mySpan.appendChild(textSpan);
+            myLink.appendChild(linkSpan);
+            myLink.style.cssText="font-weight: 500; color: blue;text-decoration:none;margin: 10px; bottom: 5px;";
 
-// adding the footer
-let myFooterDiv =document.createElement("div");
-let myFooterText = `&copy; Copyright 2021`;
-myFooterDiv.innerHTML = myFooterText;
-myFooterDiv.style.cssText = `width:100%; height:50px; text-align:center; color:white; background-color:green;display:flex;align-items:center;justify-content:center;margin-top: 50px;`;
-footer.appendChild(myFooterDiv);
-document.body.appendChild(footer);
-document.body.style.cssText = `min-height: 100vh; margin:0px;`;
+            // adding them to the main div
+            myMainDiv.appendChild(mySpan);
+            myMainDiv.appendChild(textDiv);
+            myMainDiv.appendChild(myLink);
+            content.appendChild(myMainDiv);
+            // document.body.appendChild(breaking);
+            content.style.cssText="display: grid;\n" +
+                "\tgrid-template-columns: repeat(auto-fit,minmax(200px, auto));\n" +
+                "\tjustify-content: center;\n" +
+                "\tgap: 2rem;\n" +
+                "\tmargin: 2rem;";
+        });
+        document.body.appendChild(content);
+        // adding the footer
+        let myFooterDiv =document.createElement("div");
+        let myFooterText = `&copy; Copyright 2022`;
+        myFooterDiv.innerHTML = myFooterText;
+        myFooterDiv.style.cssText = `width:100%; height:50px; text-align:center; color:white; background-color:green;display:flex;align-items:center;justify-content:center;margin-top: 50px;`;
+        footer.appendChild(myFooterDiv);
+        document.body.appendChild(footer);
+        document.body.style.cssText = `min-height: 100vh; margin:0px;`;
+
+    },100)
+}
